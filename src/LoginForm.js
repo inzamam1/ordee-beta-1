8d13e803dc2261ad29bbd9de90e1f2686877e2e3
inzamam1 <inzamamsayyed10@gmail.com>
2021-01-30 01:15:51 +0530

    cleanup and added password checker

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -2,24 +2,19 @@ import React, { useState, useEffect } from "react";
 import { db, auth } from "./firebase";
 import firebase from "firebase";
 import "./LoginForm.css";
-import { Link, Redirect,Route } from "react-router-dom";
-import Home from './Home';
-import Profile from'./Profile';
-import Settings from './Settings';
-import Support from './Support';
-import {FaGoogle} from 'react-icons/fa';
+import { Link, Redirect, Route } from "react-router-dom";
+import Home from "./Home";
+import Profile from "./Profile";
+import Settings from "./Settings";
+import Support from "./Support";
+import Password__Strength from "./Password__Strength";
 
 function LoginForm() {
   const [username, setUsername] = useState("");
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
-  const [address1,setAddress1]=useState("");
-  const [address2,setAddress2]=useState("");
-  const [state,setState]=useState("");
-  const [country,setCountry]=useState("");
-  const [postalcode,setPostalCode]=useState("");
   const [password, setPassword] = useState("");
-  const [password1,setPassword1] = useState("");
+  const [password1, setPassword1] = useState("");
   const [user, setUser] = useState(null);
 
   useEffect(() => {
@@ -42,7 +37,7 @@ function LoginForm() {
       .catch((error) => alert(error.message));
   };
   const SignUp = (event) => {
-    if(password===password1){
+    if (password === password1) {
       event.preventDefault();
       auth
         .createUserWithEmailAndPassword(email, password)
@@ -51,47 +46,46 @@ function LoginForm() {
             Name: username,
             Phone: phone,
             Email: email,
-            Address1:"",
-            Address2:'',
-            State:'',
-            Country:'',
-            PostalCode:''
+            Address1: "",
+            Address2: "",
+            State: "",
+            Country: "",
+            PostalCode: "",
           });
         })
         .catch((error) => alert(error.message));
-    } else
-      {
-        alert('Password did not match!');
-      }
-    
-   
+    } else {
+      alert("Password did not match!");
+    }
   };
 
-  const google=(event)=>{
+  const google = (event) => {
     event.preventDefault();
     var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth()
-    .signInWithPopup(provider)
-    .then((result) => {
-      /** @type {firebase.auth.OAuthCredential} */
-      var credential = result.credential;
-  
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
-      return db.collection("Restaurant").doc(result.user.uid).set({
-        Name: user.displayName,
-        Phone: '',
-        Email: user.email,
-            Address1:'',
-            Address2:'',
-            State:'',
-            Country:'',
-            PostalCode:''
-    })
-    }).catch((error) => alert(error.message));
-  }
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        /** @type {firebase.auth.OAuthCredential} */
+        var credential = result.credential;
+
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        return db.collection("Restaurant").doc(result.user.uid).set({
+          Name: user.displayName,
+          Phone: "",
+          Email: user.email,
+          Address1: "",
+          Address2: "",
+          State: "",
+          Country: "",
+          PostalCode: "",
+        });
+      })
+      .catch((error) => alert(error.message));
+  };
   const signUpButton = () => {
     document.getElementById("container").classList.add("right-panel-active");
   };
@@ -108,18 +102,17 @@ function LoginForm() {
         ) : (
           <div>
             <Route path="/" exact component={Home}>
-            <Home user={user} username={user} />
+              <Home user={user} username={user} />
             </Route>
             <Route path="/Settings" component={Settings}>
               <Settings username={user} />
             </Route>
             <Route path="/Profile" component={Profile}>
-              <Profile  user1={user} />
+              <Profile user1={user} />
             </Route>
             <Route path="/Support" component={Support}>
-              <Support  username={user} />
+              <Support username={user} />
             </Route>
-            
           </div>
         )
       ) : (
@@ -158,8 +151,9 @@ function LoginForm() {
                 value={password1}
                 onChange={(e) => setPassword1(e.target.value)}
               />
+              <Password__Strength password={password} />
               <button onClick={SignUp}>Sign Up</button>
-              </form>
+            </form>
           </div>
           <div className="form-container sign-in-container">
             <form action="#">
@@ -178,53 +172,50 @@ function LoginForm() {
                 onChange={(e) => setPassword(e.target.value)}
               />
               <a>
-                <Link to='/Password__Reset'>Forgot your password?</Link>
+                <Link to="/Password__Reset">Forgot your password?</Link>
               </a>
               <button onClick={Login}>Sign in</button>
               <br></br>
               <br></br>
               <div className="soical">
-               <ul class='social'> 
-               <li class='social'>
-                  <button id='google' onClick={google}></button>
-                  <a href="#">
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span class="fa fa-google">
-                  </span>
-                  </a> 
-                </li>
-               <li class='social'>
-                 <a href="#">
-                 <span></span>
-                 <span></span>
-                 <span></span>
-                 <span></span>
-                 <span class="fa fa-facebook"></span>
-                 </a> 
-                </li>
-                <li class='social'>
-                  <a href="#">
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span class="fa fa-instagram"></span>
-              </a> 
-                </li>
-               </ul>  
-              </div> 
+                <ul class="social">
+                  <li class="social">
+                    <button id="google" onClick={google}></button>
+                    <a href="#">
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                      <span class="fa fa-google"></span>
+                    </a>
+                  </li>
+                  <li class="social">
+                    <a href="#">
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                      <span class="fa fa-facebook"></span>
+                    </a>
+                  </li>
+                  <li class="social">
+                    <a href="#">
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                      <span class="fa fa-instagram"></span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </form>
           </div>
           <div className="overlay-container">
             <div className="overlay">
               <div className="overlay-panel overlay-left">
                 <h1>Welcome Back!</h1>
-                <p>
-                  To keep connected with us please login with your details
-                </p>
+                <p>To keep connected with us please login with your details</p>
                 <button className="ghost" id="signIn" onClick={signInButton}>
                   {/*check for id here as refreshing the page does not support animation and displays error */}
                   Sign In
@@ -243,5 +234,5 @@ function LoginForm() {
       )}
     </div>
   );
-      }
+}
 export default LoginForm;
